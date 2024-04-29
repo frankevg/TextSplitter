404287e84225c11f4af13406a6a25658b9ac6db3
Frank Edward Vegas Garcia <56662493+frankevg@users.noreply.github.com>
2024-04-29 03:35:04 -0500

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,13 @@
+with open('C:/xampp/htdocs/trash/archivoS.txt', 'r', encoding='utf-8') as f:
+    texto_completo = f.read()
+    
+max_longitud = 14500
+segmentos = []
+for i in range(0, len(texto_completo), max_longitud):
+    segmentos.append(texto_completo[i:i+max_longitud])
+
+# Bucle para escribir cada segmento en un archivo diferente
+for i, segmento in enumerate(segmentos):
+    nombre_archivo = f"segmento_{i+1}.txt"  # Genera un nombre de archivo único para cada segmento
+    with open(nombre_archivo, 'w', encoding='utf-8') as f:
+        f.write(segmento)
---
+++ src/script.py
@@ -0,0 +1,23 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+# # Crear una instancia de ChromeDriver (o de otro driver de Selenium)
+# driver = webdriver.Chrome()
+
+# # Navegar a la página web
+# driver.get("https://bignet.pe/")
+
+# # Encontrar el campo de texto y enviar un texto
+# campo_texto = driver.find_element_by_id("nombre")
+# campo_texto.send_keys("GAA")
+
+# # Enviar el formulario (si es necesario)
+# campo_texto.send_keys(Keys.RETURN)
+
+# # Cerrar el navegador
+# driver.quit()
+
+xxx= "123.1456"
+print(type(xxx))
+zzz=int(xxx)
+print(type(zzz))
